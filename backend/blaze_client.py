584e8b7c6ec8c5436d2c8ca569f7fba99504a046
Alexander <alexander.gruber@ur.de>
2026-05-27 16:17:55 +0200

    blaze_client added to project

---
+++ backend/blaze_client.py
@@ -0,0 +1,62 @@
+import requests
+
+FHIR_BLAZE_BASE = "http://localhost:8080/fhir"
+
+
+
+class FHIRBlazeClient:
+    def __init__(self, base_url):
+        self.base_url = base_url
+
+    def get_metadata(self):
+        response = requests.get(f"{self.base_url}/metadata",
+                                headers={
+                                    "Accept": "application/fhir+json"
+                                })
+        return response.json()
+    
+    def get_patientdata(self):
+        response = requests.get(f"{self.base_url}/Patient",
+                                headers={
+                                    "Accept": "application/fhir+json"
+                                })
+        return response.json()
+    
+    def get_patient_by_id(self, patient_id):
+        response = requests.get(f"{self.base_url}/Patient/{patient_id}",
+                                headers={
+                                    "Accept": "application/fhir+json"
+                                })
+        return response.json()
+    
+    def create_patient(self, patient_data):
+        response = requests.post(f"{self.base_url}/Patient", 
+                                 json=patient_data, 
+                                 headers={
+                                    "Content-Type": "application/fhir+json"
+                                })
+        return response.json()
+    
+    def update_patient(self, patient_id, patient_data):
+        response = requests.put(f"{self.base_url}/Patient/{patient_id}", 
+                                json=patient_data,
+                                headers={
+                                    "Content-Type": "application/fhir+json"
+                                })
+        return response.json()
+    
+    def delete_patient(self, patient_id):
+        response = requests.delete(f"{self.base_url}/Patient/{patient_id}")
+        return response.json
+    
+    def create_medication(self, medication_data):
+        response = requests.post(f"{self.base_url}/Medication", 
+                                 json=medication_data,
+                                 headers={
+                                     "Content-Type": "application/fhir+json"
+                                 })
+        return response.json()
+    
+    def delete_medication(self, medication_id):
+        response = requests.delete(f"{self.base_url}/Medication/{medication_id}")
+        return response.json
