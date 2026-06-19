ad3a377bb9186f25dc27b1533575d774f3910a78
Alexander <alexander.gruber@ur.de>
2026-06-19 16:56:58 +0200

    mappings modified

--- backend/mappings/Dosiereinheit.py
+++ backend/mappings/Dosiereinheit.py
@@ -33,3 +33,219 @@ DOSIEREINHEIT = {
     "u": "kg",
     "v": "mg",
 }
+
+BMP_TO_UCUM = {
+    "m": {
+    "unit": "Mio IE",
+    "system": "http://unitsofmeasure.org",
+    "code": "M[iU]",
+    },
+    "p": {
+        "unit": "IE",
+        "system": "http://unitsofmeasure.org",
+        "code": "[iU]",
+    },
+    "q": {
+        "unit": "cm",
+        "system": "http://unitsofmeasure.org",
+        "code": "cm",
+    },
+    "r": {
+        "unit": "Liter",
+        "system": "http://unitsofmeasure.org",
+        "code": "L",
+    },
+    "s": {
+        "unit": "ml",
+        "system": "http://unitsofmeasure.org",
+        "code": "mL",
+    },
+    "t": {
+        "unit": "g",
+        "system": "http://unitsofmeasure.org",
+        "code": "g",
+    },
+    "u": {
+        "unit": "kg",
+        "system": "http://unitsofmeasure.org",
+        "code": "kg",
+    },
+    "v": {
+        "unit": "mg",
+        "system": "http://unitsofmeasure.org",
+        "code": "mg",
+    },
+}
+
+BMP_TO_FHIR_QUANTITY_UNIT = {
+    "#": {
+        "unit": "Messlöffel",
+        "system": None,
+        "code": None,
+    },
+    "0": {
+        "unit": "Messbecher",
+        "system": None,
+        "code": None,
+    },
+    # "1": {
+    #     "unit": "Stück",
+    #     "system": "http://unitsofmeasure.org",
+    #     "code": "{piece}",
+    # },
+    "1": {
+        "unit": "Stück",
+        "system": None,
+        "code": None,
+    },
+    "2": {
+        "unit": "Pkg.",
+        "system": None,
+        "code": None,
+    },
+    "3": {
+        "unit": "Flasche",
+        "system": None,
+        "code": None,
+    },
+    "4": {
+        "unit": "Beutel",
+        "system": None,
+        "code": None,
+    },
+    "5": {
+        "unit": "Hub",
+        "system": None,
+        "code": None,
+    },
+    "6": {
+        "unit": "Tropfen",
+        "system": None,
+        "code": None,
+    },
+    "7": {
+        "unit": "Teelöffel",
+        "system": None,
+        "code": None,
+    },
+    "8": {
+        "unit": "Esslöffel",
+        "system": None,
+        "code": None,
+    },
+    "9": {
+        "unit": "E",
+        "system": None,
+        "code": None,
+    },
+    "a": {
+        "unit": "Tasse",
+        "system": None,
+        "code": None,
+    },
+    "b": {
+        "unit": "Applikatorfüllung",
+        "system": None,
+        "code": None,
+    },
+    "c": {
+        "unit": "Augenbadewanne",
+        "system": None,
+        "code": None,
+    },
+    "d": {
+        "unit": "Dosierbriefchen",
+        "system": None,
+        "code": None,
+    },
+    "e": {
+        "unit": "Dosierpipette",
+        "system": None,
+        "code": None,
+    },
+    "f": {
+        "unit": "Dosierspritze",
+        "system": None,
+        "code": None,
+    },
+    "g": {
+        "unit": "Einzeldosis",
+        "system": None,
+        "code": None,
+    },
+    "h": {
+        "unit": "Glas",
+        "system": None,
+        "code": None,
+    },
+    "i": {
+        "unit": "Likörglas",
+        "system": None,
+        "code": None,
+    },
+    "j": {
+        "unit": "Messkappe",
+        "system": None,
+        "code": None,
+    },
+    "k": {
+        "unit": "Messschale",
+        "system": None,
+        "code": None,
+    },
+    "l": {
+        "unit": "Mio E",
+        "system": None,
+        "code": None,
+    },
+    "m": {
+        "unit": "Mio IE",
+        "system": None,
+        "code": None,
+    },
+    "n": {
+        "unit": "Pipettenteilstrich",
+        "system": None,
+        "code": None,
+    },
+    "o": {
+        "unit": "Sprühstoß",
+        "system": None,
+        "code": None,
+    },
+    "p": {
+        "unit": "IE",
+        "system": "http://unitsofmeasure.org",
+        "code": "[iU]",
+    },
+    "q": {
+        "unit": "cm",
+        "system": "http://unitsofmeasure.org",
+        "code": "cm",
+    },
+    "r": {
+        "unit": "l",
+        "system": "http://unitsofmeasure.org",
+        "code": "L",
+    },
+    "s": {
+        "unit": "ml",
+        "system": "http://unitsofmeasure.org",
+        "code": "mL",
+    },
+    "t": {
+        "unit": "g",
+        "system": "http://unitsofmeasure.org",
+        "code": "g",
+    },
+    "u": {
+        "unit": "kg",
+        "system": "http://unitsofmeasure.org",
+        "code": "kg",
+    },
+    "v": {
+        "unit": "mg",
+        "system": "http://unitsofmeasure.org",
+        "code": "mg",
+    },
+}
