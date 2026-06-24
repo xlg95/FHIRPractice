1a9ca70954277d581c0d04868b2bab1c434d7c76
Alexander <alexander.gruber@ur.de>
2026-06-24 13:33:38 +0200

    german EventTimimng description added to every last entry

--- backend/mappings/EventTiming.py
+++ backend/mappings/EventTiming.py
@@ -1,16 +1,16 @@
 EVENT_TIMING: dict[str, list[str, str, str]] = {
     "MORN": ["Morning", "Event occurs during the morning. The exact time is unspecified and established by institution convention or patient interpretation.", "morgens"],
-    "MORN.early": ["Early Morning", "Event occurs during the early morning. The exact time is unspecified and established by institution convention or patient interpretation.", ""],
-    "MORN.late": ["Late Morning", "Event occurs during the late morning. The exact time is unspecified and established by institution convention or patient interpretation.", ""],
+    "MORN.early": ["Early Morning", "Event occurs during the early morning. The exact time is unspecified and established by institution convention or patient interpretation.", "frühmorgens"],
+    "MORN.late": ["Late Morning", "Event occurs during the late morning. The exact time is unspecified and established by institution convention or patient interpretation.", "spätmorgens"],
     "NOON": ["Noon", "Event occurs around 12:00pm. The exact time is unspecified and established by institution convention or patient interpretation.", "mittags"],
-    "AFT": ["Afternoon", "Event occurs during the afternoon. The exact time is unspecified and established by institution convention or patient interpretation.", ""],
-    "AFT.early": ["Early Afternoon", "Event occurs during the early afternoon. The exact time is unspecified and established by institution convention or patient interpretation.", ""],
-    "AFT.late": ["Late Afternoon", "Event occurs during the late afternoon. The exact time is unspecified and established by institution convention or patient interpretation.", ""],
+    "AFT": ["Afternoon", "Event occurs during the afternoon. The exact time is unspecified and established by institution convention or patient interpretation.", "nachmittags"],
+    "AFT.early": ["Early Afternoon", "Event occurs during the early afternoon. The exact time is unspecified and established by institution convention or patient interpretation.", "frühnachmittags"],
+    "AFT.late": ["Late Afternoon", "Event occurs during the late afternoon. The exact time is unspecified and established by institution convention or patient interpretation.", "spätnachmittags"],
     "EVE": ["Evening", "Event occurs during the evening. The exact time is unspecified and established by institution convention or patient interpretation.", "abends"],
-    "EVE.early": ["Early Evening", "Event occurs during the early evening. The exact time is unspecified and established by institution convention or patient interpretation.", ""],
-    "EVE.late": ["Late Evening", "Event occurs during the late evening. The exact time is unspecified and established by institution convention or patient interpretation.", ""],
-    "NIGHT": ["Night", "Event occurs during the night. The exact time is unspecified and established by institution convention or patient interpretation.", ""],
-    "PHS": ["After Sleep", "Event occurs [offset] after subject goes to sleep. The exact time is unspecified and established by institution convention or patient interpretation.", ""],
+    "EVE.early": ["Early Evening", "Event occurs during the early evening. The exact time is unspecified and established by institution convention or patient interpretation.", "frühabends"],
+    "EVE.late": ["Late Evening", "Event occurs during the late evening. The exact time is unspecified and established by institution convention or patient interpretation.", "spätabends"],
+    "NIGHT": ["Night", "Event occurs during the night. The exact time is unspecified and established by institution convention or patient interpretation.", "nachts"],
+    "PHS": ["After Sleep", "Event occurs [offset] after subject goes to sleep. The exact time is unspecified and established by institution convention or patient interpretation.", "nach dem aufstehen"],
 }
 
 BMP_KEY_TO_EVENT: dict[str, str] = {
